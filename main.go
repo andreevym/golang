2ef3635a8d8567def30a848cea9ddf2ef9fc7745
Yury Andreev <Yury.Andreev@ru.ibm.com>
2017-02-11 15:15:45 +0300

    sample with arrays
    Signed-off-by: Yury Andreev <Yury.Andreev@ru.ibm.com>

--- main.go
+++ main.go
@@ -23,6 +23,29 @@ func main() {
 
 func tryArray() {
 	fmt.Println(array_title)
-	array := [...]int32{1, 2, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096}
-	fmt.Println(array)
+
+	simpleArray := [3]int32{}
+	simpleArray[0] = 1
+	simpleArray[0] = 2
+	simpleArray[0] = 4
+	fmt.Println(simpleArray)
+
+	myArray := [...]int32{1, 2, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096}
+	fmt.Println(myArray)
+
+	firstSlice := myArray[:]
+	firstSlice = append(firstSlice, 8194)
+	fmt.Println(firstSlice)
+
+	secondSlice := make([]int32, 0)
+	secondSlice = append(secondSlice, 1)
+	secondSlice = append(secondSlice, 2)
+	secondSlice = append(secondSlice, 3)
+	fmt.Print(secondSlice)
+
+	thirdSlice := []int32{1, 2, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096}
+	thirdSlice = append(thirdSlice, 1)
+	thirdSlice = append(thirdSlice, 2)
+	thirdSlice = append(thirdSlice, 3)
+	fmt.Print(thirdSlice)
 }
